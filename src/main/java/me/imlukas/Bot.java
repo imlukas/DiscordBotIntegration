b18fdb41db81f1846e92a01da1cda8dd628ef7ea
imlukas <lppinheiro1234@gmail.com>
2022-10-13 22:37:53 +0100

    First commit, basic /server command and /roles command and welcome / leave message with hardcoded channels (lol)

--- src/main/java/me/imlukas/Bot.java
+++ src/main/java/me/imlukas/Bot.java
@@ -1,12 +1,7 @@
 package me.imlukas;
 
-import me.duncte123.botcommons.messaging.EmbedUtils;
-import me.duncte123.botcommons.web.WebUtils;
 import me.imlukas.listeners.EnterLeaveListener;
 import me.imlukas.listeners.MessageListener;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
@@ -16,9 +11,12 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
 
+import static me.imlukas.config.secrets.TOKEN;
+
+
 public class Bot {
+
     private final ShardManager shardManager;
-    private final static String TOKEN = "OTE2MTQ2NDAwMzEwNDg1MDA0.G6P5lz.lWun1-ma2K9O_nQhDf1TM4pRhM25M7_Oi8xnHA";
 
     public Bot() throws  LoginException{
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(TOKEN)
