2146f1dadbf6d24fb6fe44827ef329a1c9d75930
Lukas Pinheiro <77332031+imlukas@users.noreply.github.com>
2023-03-28 11:14:14 +0100

    bruh foocking moment

--- src/main/java/me/imlukas/config/secrets.java
+++
@@ -1,7 +0,0 @@
-package me.imlukas.config;
-
-public class secrets {
-
-    public static String DISCORD_TOKEN = "OTE2MTQ2NDAwMzEwNDg1MDA0.GZe5HE.6v-GLDzyqUvI2p7niwxhLkC_IvI1Qa7vzDEyaY";
-    public static String SPOTIFY_TOKEN = "BQDqAkn9RArnSVZV1qO3y8ehcREYu_cqp62c8t-XPEGhy8Mu0fgaAQekUaivmUxAc7hzda69V-UdaH9lLJET4VCz5mA9ruBugOZmDBCTwlJKQ0GtCmFppaLQxX02j6jM28U9dK0Au3ATwqmbF3UT_a9k4cGrgE-gnH9Q4T1atAQm4ceg46iQfp-nczu2-9o3MY2h";
-}
