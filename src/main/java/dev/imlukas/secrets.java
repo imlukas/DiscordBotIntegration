3095c963cf995c332e8342c5725134144d1ce7c0
Lukas Pinheiro <77332031+imlukas@users.noreply.github.com>
2024-03-11 16:57:00 +0000

    Delete src/main/java/dev/imlukas/secrets.java

--- src/main/java/dev/imlukas/secrets.java
+++
@@ -1,7 +0,0 @@
-package dev.imlukas;
-
-public class secrets {
-
-    public static final String DISCORD_TOKEN = "OTE2MTQ2NDAwMzEwNDg1MDA0.GgDtj_.9TFTahqc4m9vQiZ7RgfTWWfYJ_p7bO009E2qac";
-    public static final String SPOTIFY_TOKEN = "BQDqAkn9RArnSVZV1qO3y8ehcREYu_cqp62c8t-XPEGhy8Mu0fgaAQekUaivmUxAc7hzda69V-UdaH9lLJET4VCz5mA9ruBugOZmDBCTwlJKQ0GtCmFppaLQxX02j6jM28U9dK0Au3ATwqmbF3UT_a9k4cGrgE-gnH9Q4T1atAQm4ceg46iQfp-nczu2-9o3MY2h";
-}
